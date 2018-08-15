35a6a892f3f460f62a7baa8b75c386a85fb6894d
Gabriel <gabrielfrancolino@gmail.com>
2018-08-15 15:15:20 -0300

    3er commit - emitiendo desde el cliente al servidor
    emito del cliente al servidor, escucho desde el servidor.

--- server/server.js
+++ server/server.js
@@ -16,6 +16,26 @@ app.use(express.static(publicPath));
 //IO = esta es la comunicación del backend // 
 let io = socketIO(server);
 
+io.on('connection', (client) => {
+   
+    console.log('usuario conectado');
+
+    client.on('disconnect', () => {
+      console.log('usuario desconectado');
+    });
+
+    // Escuchar el cliente
+    client.on('enviarMensaje', (mensaje) => {
+        console.log(mensaje);
+    });
+
+
+});
+
+
+
+
+
 server.listen(port, (err) => {
 
     if (err) throw new Error(err);
